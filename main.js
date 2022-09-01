d7448e10ed862f3c363339573ce6e90065e96302
Beulah Mathenge <beulahmathenge@gmail.com>
2022-09-01 11:19:10 +0300

    Creating card elements

--- main.js
+++ main.js
@@ -33,3 +33,15 @@ const getDataFromDB = async () => {
     });
     // console.log(renderBooks);
   };
+
+  const createCardElements = () => {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const details = document.createElement("div");
+    const like = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    const author = document.createElement("p");
+    const ISBN = document.createElement("p");
+    const reviews = document.createElement("p");
+    return { article, img, details, like, bookTitle, author, ISBN, reviews };
+  };
